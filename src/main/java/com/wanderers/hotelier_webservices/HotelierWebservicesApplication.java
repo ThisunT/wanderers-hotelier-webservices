c64c7f6c06acdb6a40ed87ec232b8917ef422ff1
tpat4856 <tpat4856@sysco.com>
2023-11-12 15:11:45 +0530

    build: init gradle setup

---
+++ src/main/java/com/wanderers/hotelier_webservices/HotelierWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.wanderers.hotelier_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelierWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelierWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanderers/hotelier_webservices/HotelierWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderers.hotelier_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelierWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
